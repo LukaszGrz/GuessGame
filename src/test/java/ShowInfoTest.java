8f59b5b7a7a7917b2affe65980b2c97d64b36612
LukaszGrz <mr.chilli.the.hot@gmail.com>
2018-02-20 14:58:08 +0100

    klasa testująca wyświetlanie wiadomości co moim zdaniem nie ma sensu :) uznajmy ze ufamy wewnętrznym biblioteką JAVY (takim jak "sout")

---
+++ src/test/java/ShowInfoTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class ShowInfoTest {
+
+
+    @Test
+    public void tryLower() {}
+
+    @Test
+    public void tryHigher() {}
+
+    @Test
+    public void gameLose() {}
+}
