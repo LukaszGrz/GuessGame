3de899db6631231fe09f3feaace0119d9447fe34
LukaszGrz <mr.chilli.the.hot@gmail.com>
2018-02-20 14:32:40 +0100

    tworzę testy dla Validatora który będzie sprawdzać czy podana wartość jest - nullem - literałem

---
+++ src/test/java/ValidatorTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ValidatorTest {
+    @Test
+    public void canValidatorDetectNull() {
+        //given
+        IValidator validator = new ValidatorImpl();
+
+        //when
+        boolean isNull = validator.isNull(null);
+
+        //then
+        assertTrue(isNull);
+    }
+    @Test
+    public void canValidatorConfirmNotNull() {
+        //given
+        IValidator validator = new ValidatorImpl();
+
+        //when
+        boolean isNull = validator.isNull("Byleco");
+
+        //then
+        assertFalse(isNull);
+    }
+
+    @Test
+    public void canValidatorConfirmNotNumber() {
+        //given
+        IValidator validator = new ValidatorImpl();
+
+        //when
+        boolean isLiteral = validator.isLiteral("to nie jest numer");
+
+        //then
+        assertTrue(isLiteral);
+    }
+}
