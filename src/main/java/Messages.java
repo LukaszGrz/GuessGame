9f35ee6537533ff853fec5a8f7a5ecb0f9884bdc
LukaszGrz <mr.chilli.the.hot@gmail.com>
2018-02-20 15:07:57 +0100

    tu będziemy trzymać nasze wiadomości do usera

---
+++ src/main/java/Messages.java
@@ -0,0 +1,9 @@
+public class Messages {
+    public static final String winMessage = "Wygrałeś!!";
+    public static final String loseMessage = "Przegrales!!";
+    public static final String toLowMessage = "za niska liczba";
+    public static final String toHighMessage = "za wysoka liczba";
+    public static final String validationErrorMessage = "Error (sprobuj jeszcze raz oczywiście tracisz szansę)";
+    public static final String welcomeMessage = "Witaj!!!";
+    public static final String makeChoiceMessage = "Podaj liczbę";
+}
